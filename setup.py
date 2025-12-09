b15788c19a82e0fbd73a5e20d363c0947d223ed2
Muharrem Kaçkın <muharrem.kackin@strawhats.company>
2025-12-09 16:10:49 +0300

    latest project updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="certbot-dns-contabo-api",
-    version="1.0.1",
+    version="1.0.2",
     description="Contabo DNS Authenticator plugin for Certbot",
     author="Custom",
     license="Apache License 2.0",
